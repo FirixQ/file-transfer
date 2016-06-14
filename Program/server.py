1783ed9583eaedd4d79bc31e60bc172fe2d038b2
FirixQ <matthew2cooke@btinternet.com>
2016-06-14 21:52:12 +0100

    actually works now
    still cant actually send files btw

--- Program/server.py
+++ Program/server.py
@@ -23,41 +23,46 @@ online = [] #list all connect - username (same index as allCon)
 active = [] #list everyone uploading/downloading
 
 def accept(senderName, senderAddr, receiver, file):
+
     if receiver in active: #check if receiver is currently doing something
         status = 'aa'
     elif receiver in online: #check if they are online
         index = online.index(receiver)
         c = allConn[index] #get their socket
 
-        allCon[c].send(bytes(senderName + ':' + file, 'utf-8'))
-        ready = select.select([s], [], [], 120) #waits 120 seconds for a response
+        filenameInBytes = bytes(':' + file, 'utf-8') #some bytes magic
+        c.send(senderName + filenameInBytes)
+        ready = select.select([c], [], [], 120) #waits 120 seconds for a response
         if ready[0]:
             result = allCon[c].recv(1024)
         else:
-            status = 'to' #to for timeout
+            result = 'to' #to stop assignment errors
 
         if (result == 'a'): #a for accept
             status = 'a'
         elif (result == 'd'): #d for decline
             status = 'd'
+        elif (result == 'to'):
+            status = 'to'
         else:
             status = 'ue' #unknown error
     
     else:
-        status = 'na'
+        status = 'na' #not available
 
     return status
 
 def clientThread(conn): #what the client talks to
     sender = conn.recv(1024) #client username
-    online.append(sender) #add to online list
+    online.append(sender.decode('utf-8')) #add to online list
     while True:
-        active.append(sender)#mark them as active
         eRaF = conn.recv(1024).decode('utf-8').split(':') #eRaF is end receiver and file, split by colon
+        active.append(sender.decode('utf-8'))#mark them as active
         eR = eRaF[0]
         fileName = eRaF[1]
         eRStatus = accept(sender, conn, eR, fileName) #check if user can receive file
-        conn.send(bytes(eRStatus)) #send user the status
+        conn.send(bytes(eRStatus, 'utf-8')) #send user the status
+        print('About to receive file from ' + addr[0] + '(' + sender.decode('utf-8') + ')')
                 
     conn.close()
 
