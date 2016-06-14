3a59b2756b64f1383311124ff1f43f7e58ac0e11
FirixQ <matthew2cooke@btinternet.com>
2016-06-14 21:51:45 +0100

    can now accept files
    but cant download them

--- Program/client.py
+++ Program/client.py
@@ -25,10 +25,26 @@ s.send(bytes(username, 'utf-8')) #send user info to server
 
 print("Connected to server")
 
+def listen():
+    fileData = s.recv(1024) #receiver sender and file name
+    splitData = split(splitData, ':')
+    while invalid: #loop until answer is valid
+        result = input("Accept file '" + splitData[1] + "' from " + splitData[0] + "? (y/n)").lower
+        if (result == 'y'):
+            status = 'a'
+            invalid = False
+        elif (result == 'n'):
+            status = 'd'
+            invalid = False
+        else:
+            print('Invalid response')
+            invalid = True
+
+
+
 while 1:
-        
+    fileLoc = input('File path to send: ')    
     try:
-        fileLoc = input('File path to send: ')
         f = open(fileLoc)
     except (FileNotFoundError): #make sure file is accessible
         print('Invalid file location')
@@ -40,6 +56,8 @@ while 1:
     s.send(bytes(eR + ':'+ fileName, 'utf-8')) #send file name and recipitent to server
 
     eRStatus = s.recv(1024).decode('utf8') #get receiver status from server
+
+    print(eRStatus)
 
     if(eRStatus == 'a'): #send file if its accepted
         print('Sending file')
@@ -55,11 +73,18 @@ while 1:
         f.close() #close file
 
     #tell them the file cant be sent for what ever reason
-    elif(eRStatus == 'nc'):
+    elif(eRStatus == 'na'):
         print(eR + ' is not available.') 
 
     elif(eRStatus == 'd'):
         print(eR + ' declined your file.')
 
     elif(eRStatus == 'to'):
-        print(eR + ' did not accept file in time')   
+        print(eR + ' did not accept file in time') 
+    
+    elif(eRStatus == 'ue'):
+        print('Unknown server error')
+
+    elif(eRStatus == 'aa'):
+        print(eR + ' is already transferring a file')
+          
